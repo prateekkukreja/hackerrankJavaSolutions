ab1eec27d7e25563c0a6674a838aa41af7c2406d
prateek.kukreja <prateek.kukreja18@gmail.com>
2021-06-05 23:47:34 +0530

    JavaDataTypes solutions

--- src/main/java/Practice/JavaDataTypes.java
+++ src/main/java/Practice/JavaDataTypes.java
@@ -7,10 +7,11 @@ public class JavaDataTypes {
     public static void main(String[] argh) {
 
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+         int t = sc.nextInt();
 
         for (int i = 0; i < t; i++) {
             try {
+                System.out.println("Enter input to check : ");
                 long x = sc.nextLong();
                 System.out.println(x + " can be fitted in:");
                 if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE)
@@ -26,5 +27,4 @@ public class JavaDataTypes {
             }
         }
     }
-}
 }
