a8df0396c1961a60ec374de9d3c731157de38ac4
prateek.kukreja <prateek.kukreja18@gmail.com>
2021-06-06 00:15:51 +0530

    Added hackerrank EOFExample

---
+++ src/main/java/Practice/EOFExample.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+public class EOFExample {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+
+        int i = 0;
+        System.out.println("Enter input : ");
+        while (scan.hasNext()) {
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+        scan.close();
+    }
+}
